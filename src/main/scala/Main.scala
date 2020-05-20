b5799922a96ed3fe7647165fa3971228446f1403
ishrathnayeem <ishrathnayeem1@gmail.com>
2020-05-20 01:31:52 -0400

    Code to upload frequency file

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,5 @@
+import Main.hadoop
+import org.apache.hadoop.fs.Path
 
 object Main extends App with DataHadoop {
   if (hadoop.exists(stagingarea)) {
@@ -12,4 +14,9 @@ object Main extends App with DataHadoop {
     hadoop.copyFromLocalFile(calendar_dates_txt, calendar_dates)
     println("calendar_dates.txt is copied to the path")}
   else println("path of calendar_dates not found")
+}
+try {
+  hadoop
+    .copyFromLocalFile(new Path("/Users/ishrathnayeem/IntelliJ IDEA/mcit/gtfs_stm/frequencies.txt"),
+      new Path("/user/fall2019/ishrath/project4/frequencies/frequencies.txt"))
 }
