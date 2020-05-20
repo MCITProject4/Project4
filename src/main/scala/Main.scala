7796064bd30d8cb16907e7a396d8e871f2146dc3
srujan <srujan18@hotmail.com>
2020-05-20 01:30:36 -0400

    calender_dates.txt added to path

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,15 @@
+
 object Main extends App with DataHadoop {
   if (hadoop.exists(stagingarea)) {
     hadoop.copyFromLocalFile(trips_txt,trips)
+    println("trips.txt is copied to the path")
+
 }
-  else{
-    println("project4 not found")
-  }
+  else println("project4 not found")
+
+  if (hadoop.exists(calendar_dates))
+  {
+    hadoop.copyFromLocalFile(calendar_dates_txt, calendar_dates)
+    println("calendar_dates.txt is copied to the path")}
+  else println("path of calendar_dates not found")
 }
