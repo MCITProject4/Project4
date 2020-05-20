562a1eb3b67ae5e2a44d3bf1339b2c7f5d3d8c79
ishrathnayeem <ishrathnayeem1@gmail.com>
2020-05-20 00:35:30 -0400

    Added Staging Area in DataHadoop
    Updated

--- src/main/scala/DataHadoop.scala
+++ src/main/scala/DataHadoop.scala
@@ -1,3 +1,5 @@
+import java.io.FileNotFoundException
+
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{FileSystem, Path}
 
@@ -7,7 +9,7 @@ import org.apache.hadoop.fs.{FileSystem, Path}
 
     conf.addResource(new Path("/home/bd-user/opt/hadoop-2.7.3/etc/cloudera/core-site.xml"))
     conf.addResource(new Path("/home/bd-user/opt/hadoop-2.7.3/etc/cloudera/hdfs-site.xml"))
-    val fs = FileSystem.get(conf)
+    val hadoop = FileSystem.get(conf)
 
     val stagingarea = new Path("/user/fall2019/srujan/project4")
 
@@ -18,5 +20,32 @@ import org.apache.hadoop.fs.{FileSystem, Path}
     val trips_txt = new Path("/home/bd-user/Downloads/trips.txt")
     val calendar_dates_txt = new Path("/home/bd-user/Downloads/calendar_dates.txt")
     val frequencies_txt = new Path("/home/bd-user/Downloads/frequencies.txt")
+
+    if (hadoop
+      .exists(stagingarea))
+
+      try {
+        hadoop
+          .delete(stagingarea, true)
+        hadoop
+          .listStatus(stagingarea)
+        println("FAILED!! to DELETE PROJECT4 Directory")
+      }
+      catch {
+        case f: FileNotFoundException =>
+          println("PROJECT4 Directory was DELETED!\n")
+      }
+
+    try {
+      hadoop
+        .mkdirs(stagingarea)
+      hadoop
+        .listStatus(stagingarea)
+      println("PROJECT4 Directory was CREATED\n")
+    }
+    catch {
+      case f: FileNotFoundException =>
+        println("FAILED!! to CREATE PROJECT4 Directory\n")
+    }
 
 }
