b848b1f6f95164b7b802f8453979aa57b343ac2c
ishrathnayeem <ishrathnayeem1@gmail.com>
2020-05-20 01:55:30 -0400

    Updated Join

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -20,15 +20,15 @@ if (hadoop.exists(frequencies)){
 else println("path not found")
   stmt.execute("SET hive.exec.dynamic.partition.mode=nonstrict")
 
-  stmt.execute("INSERT OVERWRITE TABLE fall2019_snehith.enriched_trip PARTITION(wheelchair_accessible) "+
-    " SELECT t.route_id,t.service_id,t.trip_id,t.trip_headsign," +
-    "t.direction_id,t.shape_id,t.note_fr,t.note_en,f.start_time,f.end_time,f.headway_secs,c.date,c.exception_type,t.wheelchair_accessible "+
-    " FROM fall2019_snehith.ext_trips AS t "+
-    "FULL OUTER JOIN fall2019_snehith.ext_frequencies As f "+
-    " ON t.trip_id = f.trip_id "+
-    " FULL OUTER JOIN fall2019_snehith.ext_calendar_dates AS c "+
-    " ON t.service_id =  c.service_id " +
-    " WHERE t.wheelchair_accessible < 5 ")
+  stmt.executeUpdate(
+    """INSERT OVERWRITE TABLE fall2019_ishrath.enriched_trip PARTITION (wheelchair_accessible)
+      |SELECT t.route_id,t.service_id,t.trip_id,t.trip_headsign,t.direction_id,t.shape_id,
+      |t.note_fr,t.note_en,f.start_time,f.end_time,f.headway_secs,c.date,c.exception_type,t.wheelchair_accessible
+      |FROM ext_trips t
+      |FULL OUTER JOIN fall2019_ishrath.ext_calendar_dates c
+      |ON t.service_id = c.service_id
+      |FULL OUTER JOIN fall2019_ishrath.ext_frequencies f
+      |ON t.trip_id = f.trip_id""".stripMargin)
 
 }
 
