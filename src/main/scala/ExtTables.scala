16ef9f71c4f28050c80bba90c12b66182ebef588
ishrathnayeem <ishrathnayeem1@gmail.com>
2020-05-20 01:06:57 -0400

    Created calendar table and enriched trip

--- src/main/scala/ExtTables.scala
+++ src/main/scala/ExtTables.scala
@@ -39,4 +39,46 @@ class ExtTables {
     " location '/user/fall2019/srujan/project4/frequencies'" +
     "TBLPROPERTIES ('skip.header.line.count' = '1', 'serialization.null.format' = '')")
   println("frequencies table created")
+
+  stmt execute
+    """CREATE EXTERNAL TABLE ext_calendar_dates (
+      |service_id       STRING,
+      |date             INT,
+      |exception_type   INT
+      |)
+      |ROW FORMAT DELIMITED
+      |FIELDS TERMINATED BY ','
+      |STORED AS TEXTFILE
+      |LOCATION '/user/fall2019/ishrath/project4/calendar_dates'
+      |TBLPROPERTIES (
+      |"skip.header.line.count" = "1",
+      |"serialization.null.format" = "")""".stripMargin
+
+  println("ext_calendar_dates TABLE was CREATED\n")
+
+  stmt execute """SET hive.exec.dynamic.partition=true"""
+  stmt execute """SET hive.exec.dynamic.partition.mode=nonstrict"""
+
+  stmt execute
+    """CREATE TABLE fall2019_ishrath.enriched_trip (
+      |route_id             STRING,
+      |service_id	          STRING,
+      |trip_id	            STRING,
+      |trip_headsign       	STRING,
+      |direction_id        	INT,
+      |shape_id	            INT,
+      |note_fr             	STRING,
+      |note_en             	STRING,
+      |start_time	          TIMESTAMP,
+      |end_time	            TIMESTAMP,
+      |headway_secs        	INT,
+      |date	                INT,
+      |exception_type      	INT
+      |)
+      |PARTITIONED BY (wheelchair_accessible int)
+      |ROW FORMAT DELIMITED
+      |FIELDS TERMINATED BY ','
+      |STORED AS PARQUET""".stripMargin
+
+  println("enriched_trip TABLE was CREATED\n")
 }
