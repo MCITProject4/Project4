a38ab897c79bcd9e9a6251346318e20cce781cdd
srujan <srujan18@hotmail.com>
2020-05-20 00:13:03 -0400

    added trait file for hadoop

---
+++ src/main/scala/DataHadoop.scala
@@ -0,0 +1,22 @@
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{FileSystem, Path}
+
+  trait DataHadoop extends App {
+
+    val conf = new Configuration()
+
+    conf.addResource(new Path("/home/bd-user/opt/hadoop-2.7.3/etc/cloudera/core-site.xml"))
+    conf.addResource(new Path("/home/bd-user/opt/hadoop-2.7.3/etc/cloudera/hdfs-site.xml"))
+    val fs = FileSystem.get(conf)
+
+    val stagingarea = new Path("/user/fall2019/srujan/project4")
+
+    val trips = new Path("/user/fall2019/srujan/project4/trips")
+    val calendar_dates = new Path("/user/fall2019/srujan/project4/calendar_dates")
+    val  frequencies = new Path("/user/fall2019/srujan/project4/frequencies")
+
+    val trips_txt = new Path("/home/bd-user/Downloads/trips.txt")
+    val calendar_dates_txt = new Path("/home/bd-user/Downloads/calendar_dates.txt")
+    val frequencies_txt = new Path("/home/bd-user/Downloads/frequencies.txt")
+
+}
